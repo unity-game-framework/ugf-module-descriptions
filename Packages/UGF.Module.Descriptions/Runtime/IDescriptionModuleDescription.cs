701cb872b5fcab26516a173005da7f02f614a49b
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-11 15:30:43 +0100

    Change IDescriptionModuleDescription

--- Packages/UGF.Module.Descriptions/Runtime/IDescriptionModuleDescription.cs
+++ Packages/UGF.Module.Descriptions/Runtime/IDescriptionModuleDescription.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using UGF.Description.Runtime;
+using UGF.EditorTools.Runtime.Ids;
+
 namespace UGF.Module.Descriptions.Runtime
 {
     public interface IDescriptionModuleDescription
     {
+        IReadOnlyDictionary<GlobalId, IDescription> Descriptions { get; }
     }
 }
