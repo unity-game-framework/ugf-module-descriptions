72c4b888663d834c78210171aa7b44415af4d9fc
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-11 15:41:15 +0100

    Update DescriptionGroup.cs

--- Packages/UGF.Module.Descriptions/Runtime/DescriptionGroup.cs
+++ Packages/UGF.Module.Descriptions/Runtime/DescriptionGroup.cs
@@ -4,7 +4,7 @@ using UGF.EditorTools.Runtime.Ids;
 
 namespace UGF.Module.Descriptions.Runtime
 {
-    public class DescriptionGroup : IDescription
+    public class DescriptionGroup : DescriptionBase
     {
         public Dictionary<GlobalId, GlobalId> Descriptions { get; } = new Dictionary<GlobalId, GlobalId>();
     }
