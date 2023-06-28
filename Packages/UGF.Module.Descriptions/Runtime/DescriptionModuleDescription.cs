1a53cea788131329d4111c03fa7d4dd385345b44
Vorobey <vorobyovvorobyov@gmail.com>
2023-06-28 15:08:19 +0200

    Update DescriptionModuleDescription.cs

--- Packages/UGF.Module.Descriptions/Runtime/DescriptionModuleDescription.cs
+++ Packages/UGF.Module.Descriptions/Runtime/DescriptionModuleDescription.cs
@@ -8,12 +8,10 @@ namespace UGF.Module.Descriptions.Runtime
 {
     public class DescriptionModuleDescription : ApplicationModuleDescription, IDescriptionModuleDescription
     {
-        public Dictionary<GlobalId, IDescription> Descriptions { get; }
+        public IReadOnlyDictionary<GlobalId, IDescription> Descriptions { get; }
         public IReadOnlyList<GlobalId> LoadAsync { get; }
 
-        IReadOnlyDictionary<GlobalId, IDescription> IDescriptionModuleDescription.Descriptions { get { return Descriptions; } }
-
-        public DescriptionModuleDescription(Type registerType, Dictionary<GlobalId, IDescription> descriptions, IReadOnlyList<GlobalId> loadAsync) : base(registerType)
+        public DescriptionModuleDescription(Type registerType, IReadOnlyDictionary<GlobalId, IDescription> descriptions, IReadOnlyList<GlobalId> loadAsync) : base(registerType)
         {
             Descriptions = descriptions ?? throw new ArgumentNullException(nameof(descriptions));
             LoadAsync = loadAsync ?? throw new ArgumentNullException(nameof(loadAsync));
