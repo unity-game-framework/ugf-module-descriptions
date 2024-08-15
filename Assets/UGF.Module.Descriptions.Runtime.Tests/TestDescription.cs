626d4fdb1b36a083264f086714b86d1389c542ed
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-08-15 21:15:30 +0300

    Add tests

---
+++ Assets/UGF.Module.Descriptions.Runtime.Tests/TestDescription.cs
@@ -0,0 +1,14 @@
+﻿using UGF.Description.Runtime;
+
+namespace UGF.Module.Descriptions.Runtime.Tests
+{
+    public struct TestDescription : IDescription
+    {
+        public int Value { get; }
+
+        public TestDescription(int value)
+        {
+            Value = value;
+        }
+    }
+}
